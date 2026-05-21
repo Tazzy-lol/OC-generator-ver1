ab6b24cc5eb1eee477d33249b5c66e9f34692479
Tazzy-lol <tazzy8277@gmail.com>
2026-05-21 14:16:11 +0530

    Initial commit: Add OC generator project

---
+++ ocgenerator ver1.py
@@ -0,0 +1,14 @@
+names = ["Kael", "Luna", "Aether", "Rin"]
+powers = ["Fire manipulation", "Time control", "Shadow magic"]
+personalities = ["Chaotic", "Cold", "Energetic", "Mysterious"]
+weapons = ["Sword", "Scythe", "Chains"]
+import random
+name = random.choice(names)
+power = random.choice(powers)
+personality = random.choice(personalities)
+weapon = random.choice(weapons)
+print("=== YOUR OC ===")
+print("Name:", name)
+print("Power:", power)
+print("Personality:", personality)
+print("Weapon:", weapon)
